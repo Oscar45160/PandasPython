514e8990ce9a8dac1a3ad259c8dcb7fb2f8afd02
Omicron45 <omicron.pimpaud@gmail.com>
2022-12-09 15:57:48 +0100

    final

--- pandas.py
+++ pandas.py
@@ -1,5 +1,18 @@
 import pandas as pd
+import random
+import numpy as np
 
-df = pd.read_csv('db/topic.csv')
+student = 'db/student.csv'
+topic = 'db/topic.csv'
 
-print(df.to_string())
+df_1 = pd.read_csv(student, header=0)
+df_2 = pd.read_csv(topic, header=0)
+association = pd.concat([df_1, df_2], axis=1)
+
+print(association)
+
+mylist = [
+  'Tayeb', 'Oscar', 'Antoine', 'Anis', 'Victor', 'Souleyman', 'Etienne', 'Nael'
+]
+
+print(random.choice(mylist))
